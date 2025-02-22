5d8e2bf3d546706dd873af28ef68a32ff8eb0947
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-02-22 17:40:35 +0530

    useState() hook used to get and store savedTasks from localStorage().

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -3,7 +3,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  // to get and store saved tasks
+  const [tasks, setTasks] = useState(()=>{
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+
+    
+  })
   return (
     <>
       <h1>Just Do It</h1>
